6fa7a9e6aa56093f06248200b63608dbe8f872d2
xPinkPlaguex <156326946+xPinkPlaguex@users.noreply.github.com>
2025-10-30 14:22:54 +0100

    Update MrWhiteSite.tsx

--- src/MrWhiteSite.tsx
+++ src/MrWhiteSite.tsx
@@ -112,7 +112,7 @@ export default function MrWhiteSite() {
           <img src={BASE_URL + "aa.svg"} alt="Mr White logó" className="h-[3.75rem] md:h-[4.375rem] w-auto block" loading="eager" decoding="async" />
         </a>
 <nav
-  className="flex items-center gap-8 text-[14px] md:text-[16px]"
+  className="flex items-center gap-8 text-[12px] md:text-[14px]"
   style={{
     fontFamily: "'Aboreto', serif",
     textTransform: "uppercase",
